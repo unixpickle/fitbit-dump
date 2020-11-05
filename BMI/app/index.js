1c8e6ea0eadc4b5cd5a1cbda1401aa7b9d27c8ca
Alex Nichol <unixpickle@gmail.com>
2020-11-05 13:09:48 -0500

    start working on a BMI calculator

---
+++ BMI/app/index.js
@@ -0,0 +1,58 @@
+import { user } from "user-profile";
+import document from "document";
+
+const bmiValue = document.getElementById('bmi-value');
+const weightValue = document.getElementById('weight-value');
+const heightValue = document.getElementById('height-value');
+
+const addHeight = document.getElementById('add-height');
+const subHeight = document.getElementById('sub-height');
+const addWeight = document.getElementById('add-height');
+const subWeight = document.getElementById('sub-height');
+
+let currentWeight = 150;
+let currentHeight = 5*12 + 7;
+
+function updateFields() {
+  weightValue.text = currentWeight + ' lbs';
+  heightValue.text = Math.floor(currentHeight / 12) + "' " + (currentHeight % 12) + '"';
+
+  const kg = currentWeight * 0.453592;
+  const meters = currentHeight * 0.0254;
+  const bmi = kg / (meters * meters);
+  bmiValue.text = bmi.toFixed(1);
+}
+
+function loadStats() {
+  if (user.height) {
+    currentHeight = Math.round(user.height / 0.0254);
+  }
+  if (user.weight) {
+    currentWeight = Math.round(user.weight / 0.453592);
+  }
+}
+
+addHeight.addEventListener('click', () => {
+  currentHeight++;
+  updateFields();
+});
+subHeight.addEventListener('click', () => {
+  if (currentHeight > 1) {
+    currentHeight--;
+    updateFields();
+  }
+});
+
+addWeight.addEventListener('click', () => {
+  currentWeight++;
+  updateFields();
+});
+subWeight.addEventListener('click', () => {
+  if (currentWeight > 0) {
+    currentWeight--;
+    updateFields();
+  }
+});
+
+loadStats();
+updateFields();
